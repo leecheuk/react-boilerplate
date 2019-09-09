2c489f4c5c135ea5808bb71a2f59652e7bf6628b
m <dev.cheuk.lee@gmail.com>
2019-09-09 02:58:05 -0400

    fixed store state mapping to props

--- src/containers/ActionButton.tsx
+++ src/containers/ActionButton.tsx
@@ -1,17 +1,14 @@
 import ActionButton from '../components/ActionButton';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
-import { NotificationState, Notification, NotificationActionTypes } from '../constants/actionTypes';
+import { StoreState, Notification, NotificationActionTypes } from '../constants/actionTypes';
 import { setNotification, clearNotification } from '../actions';
 
 
-const mapStateToProps = (state: NotificationState) => ({
-    notification: state
+const mapStateToProps = (state: StoreState) => ({
+    notification: state.notification
 });
 
-// const mapDispatchToProps = (dispatch : Dispatch<NotificationActionTypes>) => ({
-//     setNotification: (notification: Notification) => dispatch(setNotification(notification))
-// })
 const mapDispatchToProps = (dispatch: Dispatch<NotificationActionTypes>) => {
     return {
         setNotification: (notification: Notification) => {dispatch(setNotification(notification))},
