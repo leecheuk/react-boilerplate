c3c477bbc4b3219deed09a97d5421b0b99e72949
m <dev.cheuk.lee@gmail.com>
2019-09-09 02:39:52 -0400

    added ActionButton to view

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,14 @@
 import React, {Component} from "react";
 import {Link} from 'react-router-dom';
+import ActionButton from '../containers/ActionButton';
 
 class Home extends Component {
     render() {
         return (
             <>
                 <p>Home Page</p>
+                <ActionButton/>
+                <br/>
                 <Link to="/about">About Us</Link>
             </>
         )
