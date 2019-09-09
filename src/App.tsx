d894f1f4c4874624b578e62b26daa174eb679b6c
m <dev.cheuk.lee@gmail.com>
2019-09-09 02:44:01 -0400

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,13 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import router from './router';
 // styles
 import "./App.scss"
+// containers
+import NotificationAlert from './containers/NotificationAlert';
 
 const App: React.FC = () => {
   return (
     <Router>
+      <NotificationAlert/>
       {router}
     </Router>
   );
